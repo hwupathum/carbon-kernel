fb833e7ee035e86f0ff0d635eabec17a721ae514
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-04-11 09:57:26 +0530

    Add guava cache

--- core/org.wso2.carbon.http.client/src/main/java/org/wso2/carbon/http/client/ClientUtils.java
+++ core/org.wso2.carbon.http.client/src/main/java/org/wso2/carbon/http/client/ClientUtils.java
@@ -1,18 +1,41 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.http.client;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.wso2.carbon.utils.HTTPClientUtils;
 
 public class ClientUtils {
+
+    private static final Log log = LogFactory.getLog(ClientUtils.class);
 
     private ClientUtils() {
     }
 
-    public static CloseableHttpClient createClientWithCustomVerifier() {
+    public static CloseableHttpClient createClient() {
 
-        HttpClientBuilder httpClientBuilder = HTTPClientUtils.createClientWithCustomVerifier();
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create().useSystemProperties();
+
+        log.info("Creating a new HttpClient instance");
 
         return httpClientBuilder.build();
+
     }
 }
---
+++ core/org.wso2.carbon.http.client/src/main/java/org/wso2/carbon/http/client/cache/HttpClientCache.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.http.client.cache;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import java.io.IOException;
+
+public class HttpClientCache extends ClientBaseCache<String, CloseableHttpClient> {
+
+    private static final Log log = LogFactory.getLog(HttpClientCache.class);
+    private static HttpClientCache instance;
+
+
+    private HttpClientCache(int cacheSize, int expireAfterAccess) {
+
+        super(cacheSize, expireAfterAccess, notification -> {
+            try {
+                if (notification.getValue() != null) {
+                    notification.getValue().close();
+                    log.info("Http Client - " + notification.getKey() + " removed due to " +
+                            notification.getCause());
+                }
+            } catch (IOException e) {
+                log.error("Error occurred while closing the http client for key: " + notification.getKey(), e);
+            }
+        });
+    }
+
+    public static HttpClientCache getInstance() {
+
+        if (instance == null) {
+            int ttlMinutes = 1;
+            int cacheTimeoutMinutes = 5;
+            instance = new HttpClientCache(100, ttlMinutes + cacheTimeoutMinutes);
+        }
+        return instance;
+    }
+}
